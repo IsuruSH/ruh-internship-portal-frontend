f1689e5bb99b1dbecd5c39ed51a6c3fb89c5bbc4
IsuruSH <shanakaisuru11@gmail.com>
2025-01-05 11:25:41 +0530

    Increase close button size in login page for better visibility

--- app/pages/login/page.jsx
+++ app/pages/login/page.jsx
@@ -88,7 +88,7 @@ const Page = ({ onClose, initialMode }) => {
               {/* Close Button */}
               <button
                 onClick={onClose}
-                className="absolute top-2 right-4 text-gray-500 hover:text-gray-700"
+                className="text-2xl absolute top-2 right-4 text-gray-500 hover:text-gray-700"
               >
                 &times;
               </button>
@@ -227,7 +227,7 @@ const Page = ({ onClose, initialMode }) => {
               {/* Close Button */}
               <button
                 onClick={onClose}
-                className="absolute top-2 right-4 text-gray-500 hover:text-gray-700"
+                className="text-2xl absolute top-2 right-4 text-gray-500 hover:text-gray-700"
               >
                 &times;
               </button>
